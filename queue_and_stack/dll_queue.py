82b80d680bbf38207f74f99c6a3ce7492f004e8c
Michael Hart <michael@mhart.dev>
2019-12-17 12:28:27 -0500

    Queue: removed unnecessary duplication of length functionality

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -3,16 +3,13 @@ from doubly_linked_list import DoublyLinkedList
 
 class Queue:
     def __init__(self):
-        self.size = 0
         self.storage = DoublyLinkedList()
 
     def enqueue(self, value):
-        self.size += 1
         self.storage.add_to_tail(value)
 
     def dequeue(self):
-        if self.size > 0:
-            self.size -= 1
+        if self.storage.length > 0:
             return self.storage.remove_from_head()
         else:
             return None
